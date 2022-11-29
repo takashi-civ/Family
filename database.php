2d965434766bf8123c57f7102e70a494acad250c
takashi-civ <119102151+takashi-civ@users.noreply.github.com>
2022-11-30 02:26:38 +0900

    commit-database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'takashi');
-define('DB_PASSWD', '@Cal150@221');
+define('DB_USER', 'username');
+define('DB_PASSWD', 'password');
 define('DB_NAME', 'Family'); ?>
